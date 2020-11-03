734ba4bc1f5b2cf7e281950d81a1cbd521f4f099
Maciej Ostrowski <maciej@dekode.no>
2020-11-03 13:46:03 +0100

    buymeacoffee link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/header.component";
 import CardContainer from "./components/CardContainer/cardcontainer.component"
+import CoffeeButton from "./components/CoffeeButton/coffee.component";
 
 function App () {
   return (
     <div>
       <Header />
       <CardContainer />
+      <CoffeeButton />
     </div>
   );
 }
---
+++ src/components/CoffeeButton/coffee.component.jsx
@@ -0,0 +1,14 @@
+import './coffee.style.scss'
+
+const CoffeeButton = () => {
+  return (
+    <a href='https://www.buymeacoffee.com/maaaaciej' rel="noreferrer"  target="_blank">
+      <div className='coffee'>
+        <span className='coffee-header'>Liker du siden?</span>
+        <span>Spander en kaffe på meg!</span>
+      </div>
+    </a>
+  )
+}
+
+export default CoffeeButton
